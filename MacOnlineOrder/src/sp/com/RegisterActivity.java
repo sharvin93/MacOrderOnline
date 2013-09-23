45d3696e5c2a4fedcd010649fbb6aefa440ed9f7
hzhiguang <rune199@hotmail.com>
2013-09-23 22:25:59 +0800

    ascasc

---
+++ MacOnlineOrder/src/sp/com/RegisterActivity.java
@@ -0,0 +1,19 @@
+package sp.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		
+		
+		
+	}
+	
+
+}
